54358a7880db650927fa2b400ee3ca46cd7a1fb4
tordesa <odesapunio@gmail.com>
2022-07-03 02:18:54 +0100

    Ch01 complete

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_africa, false),
+        Question(R.string.question_asia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_americas, true)
+    )
+
+    private val currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        falseButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package com.example.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question (@StringRes val textRestI: Int, val answer: Boolean)
